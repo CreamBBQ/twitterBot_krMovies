cf87dc755d735b9eb6a14d333ac88054475e8ac1
CreamBBQ <jgmd1306@gmail.com>
2022-02-20 17:13:30 -0300

    Web scraping to IDBM is ready

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from googletrans import Translator
 translator = Translator()
 
 
+#This script save important information about Korean films from a list in IDBM, with this information
+#i'm going to have a Twitter bot that tweet a Korean film every day.
 #I'm going to ignore movies that have a summary that's too long because tweets have a character limit 
 TITLE_PATH = '//div[@class="lister-item-content"]/p[@class="" and not(contains(.,"...                ")) and not(contains(.,"\n        ")) and not(contains(.," »\n"))]/../..//div[@class="ipl-rating-star small"]/../../h3[@class="lister-item-header"]/a/text()'
 ABSTRACT_PATH  = '//div[@class="ipl-rating-star small"]/../..//p[@class="" and not(contains(.,"...                ")) and not(contains(.,"\n        ")) and not(contains(.," »\n"))]/text()'
 YEAR_PATH = '//div[@class="lister-item-content"]/p[@class="" and not(contains(.,"...                ")) and not(contains(.,"\n        ")) and not(contains(.," »\n"))]/../..//div[@class="ipl-rating-star small"]/../../h3[@class="lister-item-header"]/span[@class="lister-item-year text-muted unbold"]/text()'
 STARS_PATH = '//div[@class="lister-item-content"]/p[@class="" and not(contains(.,"...                ")) and not(contains(.,"\n        ")) and not(contains(.," »\n"))]/../div[@class="ipl-rating-widget"]/div[@class="ipl-rating-star small"]/span[@class="ipl-rating-star__rating"]/text()'
-page1 = 'https://www.imdb.com/list/ls052519910/?sort=list_order,asc&st_dt=&mode=detail&page=1'
 #'https://www.imdb.com/list/ls052519910/?sort=list_order,asc&st_dt=&mode=detail&page=n'
 #This link takes you to the n list of movies. Every list had 100 movies. There are 19 list and 1983 movies.   »\n
 
@@ -34,8 +35,7 @@ def get_list_info(some_link):
             year_list = parsed.xpath(YEAR_PATH)
             stars_list = parsed.xpath(STARS_PATH)
             abstract_list = translation(parsed.xpath(ABSTRACT_PATH))
-            #abstract_list = parsed.xpath(ABSTRACT_PATH)
-            for iteration, elemento in enumerate(title_list):
+            for iteration, element in enumerate(title_list):
                 temporal_dic = {}
                 temporal_dic = {
                     "name" : title_list[iteration], 
